da09fc109071e5f8d3780e6ba2b85c2dc40f2091
Mihir Nikam <63261772+mihir7121@users.noreply.github.com>
2021-06-20 12:58:55 +0530

    Update settings.py

--- BackEnd/EmoFy/settings.py
+++ BackEnd/EmoFy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hl)3tounv#lbwuw_!r^5uw15&onpvsow_pj9iabo-$t!e4v$**'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
